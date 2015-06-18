bc1e9fc617baedaf43b562f2dd426dbe43b89d93
Marcus A. Moore <marcus.moore.webdev@gmail.com>
2015-06-18 11:44:06 -0700

    put <main> around content

--- index.php
+++ index.php
@@ -59,56 +59,58 @@
         </div>
 
       </div>
+      <main>
+        <div class="row">
+          <div class="col-lg-6">
+            <iframe width="300" height="250" src="https://www.youtube.com/embed/YC-CtcUDTPg?feature=player_embedded" frameborder="0" allowfullscreen></iframe>
+            <p>UMASDA address:</p>
+            <p>339 Maple St, Unit C, Salinas, CA 93901</p>
+            <br>
+            <p>Adult Training Hours: Sun 7:30PM, Tues 7PM, Thurs 7PM, Sat 10AM</p>
+            <p>Children & Young Adults 14+ by Appointment Only</p>
+            <br>
+            <p>Steve Zalazowski | 831 442-9472 |</p><p>   |  <a title="mailto:steve@umasda.com" href="mailto:steve@umasda.com">Steve@umasda.com</a></p>
+            <p>Adam Noggle | 831 595-9421 | </p><p> <a title="mailto:ANoggle@umasda.com" href="mailto:ANoggle@umasda.com">ANoggle@umasda.com</a></p>
+  ￼          <?php include "UMASDA_Facebook_badge.php" ?>
+            <br>
+            <?php include "ma_lineage.php" ?>
+            <br>
+            <?php include "google_syndication.php" ?>
+          </div>
 
-      <div class="row">
-        <div class="col-lg-6">
-          <iframe width="300" height="250" src="https://www.youtube.com/embed/YC-CtcUDTPg?feature=player_embedded" frameborder="0" allowfullscreen></iframe>
-          <p>UMASDA address:</p>
-          <p>339 Maple St, Unit C, Salinas, CA 93901</p>
-          <br>
-          <p>Adult Training Hours: Sun 7:30PM, Tues 7PM, Thurs 7PM, Sat 10AM</p>
-          <p>Children & Young Adults 14+ by Appointment Only</p>
-          <br>
-          <p>Steve Zalazowski | 831 442-9472 |</p><p>   |  <a title="mailto:steve@umasda.com" href="mailto:steve@umasda.com">Steve@umasda.com</a></p>
-          <p>Adam Noggle | 831 595-9421 | </p><p> <a title="mailto:ANoggle@umasda.com" href="mailto:ANoggle@umasda.com">ANoggle@umasda.com</a></p>
-￼          <?php include "UMASDA_Facebook_badge.php" ?>
-          <br>
-          <?php include "ma_lineage.php" ?>
-          <br>
-          <?php include "google_syndication.php" ?>
+          <div class="col-lg-6">
+            <p>Adult Self Defense - Group & Private Lessons available</p>
+            <br>
+            <p>Young Adults - 14+ w/Parent On-Site by Appointment Only</p>
+            <br>
+            <p>Self Defense Training - Go Home Safe!</p>
+            <br>
+            <p>Learn to use your body and your natural abilities to defend yourself and your loved ones!</p>
+            <br>
+            <p>Self Defense vs. Common Street Attacks:</p>
+
+              <ul>
+                <li>Punches</li>
+                <li>Grabs</li>
+                <li>Tackles</li>
+                <li>Bear Hugs/Takedowns</li>
+                <li>Kicks</li>
+                <li>Bladed & Non-bladed Weapons</li>
+              </ul>
+
+            <p>Psychological Preparation & Post-Encounter Recovery</p>
+            <br>
+            <p>All Instructors are required to re-certify their credentials annually.</p>
+            <br>
+            <p>All members of UMASDA are bound by the <a href="bylaws.php"> UMASDA Bylaws</a>.</p>
+            <br>
+            <?php include "Amazon_charity_banner.php" ?>
+            <br>
+
+          </div>
         </div>
 
-        <div class="col-lg-6">
-          <p>Adult Self Defense - Group & Private Lessons available</p>
-          <br>
-          <p>Young Adults - 14+ w/Parent On-Site by Appointment Only</p>
-          <br>
-          <p>Self Defense Training - Go Home Safe!</p>
-          <br>
-          <p>Learn to use your body and your natural abilities to defend yourself and your loved ones!</p>
-          <br>
-          <p>Self Defense vs. Common Street Attacks:</p>
-
-            <ul>
-              <li>Punches</li>
-              <li>Grabs</li>
-              <li>Tackles</li>
-              <li>Bear Hugs/Takedowns</li>
-              <li>Kicks</li>
-              <li>Bladed & Non-bladed Weapons</li>
-            </ul>
-
-          <p>Psychological Preparation & Post-Encounter Recovery</p>
-          <br>
-          <p>All Instructors are required to re-certify their credentials annually.</p>
-          <br>
-          <p>All members of UMASDA are bound by the <a href="bylaws.php"> UMASDA Bylaws</a>.</p>
-          <br>
-          <?php include "Amazon_charity_banner.php" ?>
-          <br>
-
-        </div>
-      </div>
+      </main>
 
       <div class="row">
 
