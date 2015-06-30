0b1d7a0013a1d8ced8fc5414598d5cded6dbaf34
Marcus A. Moore <marcus.moore.webdev@gmail.com>
2015-06-29 23:49:26 -0700

    add wiley.php and editor note to css

---
+++ wiley.php
@@ -0,0 +1,138 @@
+<!doctype html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang=""> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8" lang=""> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9" lang=""> <![endif]-->
+<!--[if gt IE 8]><!-->
+
+<html class="no-js" lang="en"> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title>UMASDA Staff</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="apple-touch-icon" href="apple-touch-icon.png">
+
+
+        <link rel="stylesheet" href="css/bootstrap-theme.min.css">
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <script src="js/vendor/bootstrap.min.js"></script>
+        <link rel="stylesheet" href="css/main.css">
+        <style media="screen">
+          h3 {font-size:14pt}
+        </style>
+
+        <script src="js/vendor/modernizr-2.8.3-respond-1.4.2.min.js"></script>
+    </head>
+    <body >
+        <!--[if lt IE 8]>
+            <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+
+    <div class="container-fluid" >
+
+      <div class="row">
+
+        <div class="col-lg-12" >
+          <img  class="img-responsive" src="img/2011banner.jpg" alt="United Martial Arts Self Defense Academy"  />
+        </div>
+
+      </div>
+
+      <div class="row">
+
+        <div class="col-lg-12" id="nav">
+
+          <?php include 'nav.php' ?>
+
+        </div>
+
+      </div>
+
+      <div class="row">
+
+        <div class="col-lg-12" >
+          <h1>Chris "AC" Wiley</h1>
+          <h2>UMASDA Advisor</h2>
+          <h3>Autobiography of Chris "AC" Wiley</h3>
+        </div>
+
+      </div>
+
+      <div class="row">
+        <div class="col-lg-6">
+          <img src="img/Wiley.png" alt="Chris "AC" Wiley" />
+        </div>
+        <div class="col-lg-6">
+          <p>
+            My heroes have always been cowboys of one type or another.
+          </p>
+          <p>
+            When I was a kid, I was asked what I wanted to be when I grow up. First of all, that’s a messed up question.  A kid doesn’t even know what they want to have for lunch, let alone what they want to be when they grow up.  Don’t ask kids that question.
+          </p>
+          <p>
+            Anyway, I said that I wanted to be a superhero, so I could fly.  Then I was introduced to GI Joe and Snake Eyes.  The idea of being a martial arts master that was also a sneaky, bad-ass Soldier caught my attention and set it ablaze.  So what if he couldn’t fly…he fit into a helicopter, so that was pretty close.  As I continued to grow, I would have a plethora of toy guns, swords, knives, and the like all over the house and would spend my summers playing GI Joe ninja at my house and at the houses of my friends.
+          </p>
+
+
+        </div>
+
+       </div>
+       <div class="row">
+         <div class="col-lg-12">
+           <p>
+             Fast forward to the early 90’s.  My interest in martial arts had only continued to grow.  One day, I find out that a friend of mine would be taking Karate.  This began a multiple month long conversation with my parents.  On a Tuesday in March, 1994 I took my first class in what I have nicknamed “American Gojukan Karate.”  It is a blend of Shotokan forms and stances, Goju Ryu philosophy, an extremely healthy portion of ground-n-pound (before MMA took off, by the way), and street-level aikijutsu and Judo.  It was through my first sensei, Fred Vincent that I would start to become the martial artist that I am.  The classes were hard, the tests near impossible, and the rewards amazing.
+           </p>
+           <p>
+             After earning a commission as a US Army aviator (helicopter pilot), I went to flight school in Alabama where I began a study of Taekwondo in order to improve my kicks.  I built a lifelong friendship with my instructor there, Jimmie Cole, and earned a 1st Dan in that system.  While in AL, I was first introduced to the IKCA Chinese Kenpo system.  I was eventually assigned to a post in TN where I continued my studies, this time in an Aikijutsu-Shotokan fusion under Bill Kellett, earning my 3rd Dan and Menkyo (teaching license). Deploying to Iraq thrice, the middle trip I established a karate class that ran 4 days a week for a year.  I then was stationed in NY and had the distinct pleasure of training with the IKCA’s Andrew Croniser, whose passion and technical expertise in his chosen art (IKCA Chinese Kenpo) is truly inspiring.  I continued my kenpo training under Andrew and eventually earned my black belt, and with recognition of previous time in the arts and based on my testing scores up to black belt In accordance with the IKCA bylaws, was presented my 3rd Dan.  Today I am still an active duty Army officer, currently stationed in WA state, a husband and father of 2, and a member of the largest independent traditional Motorcycle Club on the planet.
+           </p>
+           <p>
+             If asked today, I would probably describe my “personal system” as American Chu’an Fa (Fist Way).  I owe a lifetime of loyalty to my instructors, especially within my Kenpo lineage (like Chuck Sullivan and Vic LeRoux) who have provided me so much more than I can ever repay them for.
+           </p>
+           <p>
+             Over the past two decades I have studied Shotokan and Goju Ryu karate, both IKCA Chinese Kenpo and American Kenpo, Taekwondo, Aikido, several forms of US Military combatives, and Krav Maga.  I have also had varying levels of exposure to Muay Thai, Judo, Jujutsu, Ninpo, Jeet Kun Do, and Kobudo.   I can honestly say that I have done something related to martial arts every single day of my life since that Tuesday in March. Recently, my friend Steve asked me to share my experience as an advisor to UMASDA and I am honored to accept his offer.  Oh, and I hate Adam.
+           </p>
+           <p class="editor-note">
+             Editor’s Note: He doesn’t really hate Adam. But The Hat does.
+           </p>
+
+
+         </div>
+       </div>
+
+
+
+      <div class="row">
+
+        <div class="col-lg-12" >
+
+          <?php include 'footer.php' ?>
+        </div>
+
+
+      </div>
+
+
+    </div> <!-- /container-fluid -->
+
+
+
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.2.min.js"><\/script>')</script>
+
+        <script src="js/vendor/bootstrap.min.js"></script>
+
+        <script src="js/plugins.js"></script>
+        <script src="js/main.js"></script>
+
+        <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
+        <script>
+            (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
+            function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
+            e=o.createElement(i);r=o.getElementsByTagName(i)[0];
+            e.src='//www.google-analytics.com/analytics.js';
+            r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
+            ga('create','UA-XXXXX-X','auto');ga('send','pageview');
+        </script>
+    </body>
+</html>
